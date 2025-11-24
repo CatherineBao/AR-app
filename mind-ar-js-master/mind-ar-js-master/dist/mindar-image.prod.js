7f252172d227255ba40e1502e5d6fa0ef81394f0
Catherine Bao <112794244+CatherineBao@users.noreply.github.com>
2025-11-24 19:12:04 +0800

    dist

---
+++ mind-ar-js-master/mind-ar-js-master/dist/mindar-image.prod.js
@@ -0,0 +1,15 @@
+import { C as o, a as r } from "./controller-FCXGhmSm.js";
+import { U as i } from "./ui--lfYp8Dx.js";
+import "react";
+import "react-dom/client";
+window.MINDAR || (window.MINDAR = {});
+window.MINDAR.IMAGE = {
+  Controller: o,
+  Compiler: r,
+  UI: i
+};
+export {
+  r as Compiler,
+  o as Controller,
+  i as UI
+};
